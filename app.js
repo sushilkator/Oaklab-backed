947a81ac318fbf7023ff12583c63493251fa47fb
sushilkator <sukumar@woundtech.net>
2021-09-13 09:33:28 +0530

    code clean up

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const https = require('https');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/progress');
 
@@ -28,11 +27,8 @@ app.use(function(req, res, next) {
 
 
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
